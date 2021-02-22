32410c5017ad6fa1501a91e6dc3ab550e6d58892
Samuel Vieira <vieira.samuel@outlook.com.br>
2021-02-22 16:18:07 -0300

    primeira aula

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from './styles/components/ExperienceBar';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
